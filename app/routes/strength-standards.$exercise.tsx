c061f139f7f0c59cd04a4c3bf015be67d2ea66b7
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2023-11-12 16:12:49 -0500

    asd

--- app/routes/strength-standards.$exercise.tsx
+++ app/routes/strength-standards.$exercise.tsx
@@ -25,41 +25,52 @@ const columns: ColumnsType<BenchPressDataRow> = [
     title: "Untrained",
     dataIndex: "untrained",
     key: "untrained",
-    render: (text) => <Tag color="#8bbb11">{text}</Tag>,
+    render: (text) => <Tag color="#8bbb11">{text} lbs</Tag>,
   },
   {
     title: "Novice",
     dataIndex: "novice",
     key: "novice",
-    render: (text) => <Tag color="#a8c1f8">{text}</Tag>,
+    render: (text) => <Tag color="#a8c1f8">{text} lbs</Tag>,
   },
   {
     title: "Intermediate",
     dataIndex: "intermediate",
     key: "intermediate",
-    render: (text) => <Tag color="#7f9ef3">{text}</Tag>,
+    render: (text) => <Tag color="#7f9ef3">{text} lbs</Tag>,
   },
   {
     title: "Advanced",
     dataIndex: "advanced",
     key: "advanced",
-    render: (text) => <Tag color="#5273e0">{text}</Tag>,
+    render: (text) => <Tag color="#5273e0">{text} lbs</Tag>,
   },
   {
     title: "Elite",
     dataIndex: "elite",
     key: "elite",
-    render: (text) => <Tag color="#2b4acb">{text}</Tag>,
+    render: (text) => <Tag color="#2b4acb">{text} lbs</Tag>,
   },
   {
     title: "Godly Strength",
     dataIndex: "godlyStrength",
     key: "godlyStrength",
-    render: (text) => <Tag color="#d89614">{text}</Tag>,
+    render: (text) => <Tag color="#d89614">{text} lbs</Tag>,
   },
 ];
 
-const data: BenchPressDataRow[] = [145, 180, 200, 250].map((bodyWeight) => {
+const startWeight = 130;
+const endWeight = 250;
+const increment = 5;
+
+const weightArray = Array.from(
+  { length: Math.ceil((endWeight - startWeight) / increment) + 1 },
+  (_, index) => startWeight + index * increment
+);
+
+console.log(weightArray);
+
+const data: BenchPressDataRow[] = weightArray.map((bodyWeight) => {
   const calculateOneRepMax = (trainingLevelIndex: number) => {
     const result = calculateOneRepMaxRequired(
       30,
@@ -100,8 +111,24 @@ export default function StrengthStandardByExercise() {
       <Col span={24}>
         <Typography.Title level={2}>{exercise?.toUpperCase()}</Typography.Title>
       </Col>
+      <Col span={24}>
+        <Typography.Text>
+          Welcome to the realm of the Iron Giants, where mere mortals look up to
+          you with a mix of awe and confusion. You've transcended the ordinary,
+          turning the gym into your personal playground. As an elite lifter,
+          you're not just lifting weights; you're orchestrating a symphony of
+          strength that leaves everyone else wondering if they accidentally
+          stumbled into a superhero convention.
+        </Typography.Text>
+        <br />
+        <Typography.Title level={4}>Bench Press - Adult Men</Typography.Title>
+      </Col>
       <Col span={12}>
-        <Table columns={columns} dataSource={data} />
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={{ pageSize: 6, defaultCurrent: 2 }}
+        />
       </Col>
     </Row>
   );
